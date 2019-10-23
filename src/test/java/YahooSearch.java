b6611a4887d10ff14ed0569b31e009c681ab15b1
Aleksandr-Belski <belski.alex@gmail.com>
2019-10-23 00:14:54 -0700

    Yahoo search complete

--- src/test/java/YahooSearch.java
+++ src/test/java/YahooSearch.java
@@ -1,17 +1,67 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class YahooSearch {
 
+    WebDriver driver;
 
+    @AfterSuite
+    public void closingAfterSuite() {
+        driver.close();
+
+    }
 
     @Test
     public void testName00001() {
         System.setProperty("webdriver.gecko.driver", "/Users/aleksandrbelski/IdeaProjects/sepJavaTestNG/src/test/recources/drivers/geckodriver");
-        WebDriver driver = new FirefoxDriver();
-        driver.get("https://yahoo.com");
 
+        openingBrowser();
+        navigateToYahoo();
+        typeSearchText();
+        clickMagnifierButton();
+        waitingForPageLoading();
+        assertResultsPage();
+    }
+
+    private void assertResultsPage() {
+        WebElement paginationAtBottom = driver.findElement(By.className("compPagination"));
+        boolean isDisplayed = paginationAtBottom.isDisplayed();
+        boolean expected = true;
+        Assert.assertEquals(isDisplayed, expected);
+    }
+
+    private void waitingForPageLoading() {
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+    }
+
+    private void clickMagnifierButton() {
+        String magnifier = "#uh-search-button";
+        driver.findElement(By.cssSelector(magnifier)).click();
+    }
+
+
+    private void typeSearchText() {
+        String selector = "#uh-search-box";
+        WebElement webElement = driver.findElement(By.cssSelector(selector));
+        webElement.sendKeys("portnov school");
+    }
+
+
+
+    private void navigateToYahoo() {
+        driver.get("https://yahoo.com");
+    }
+
+    private void openingBrowser() {
+        driver= new FirefoxDriver();
 
     }
 }
